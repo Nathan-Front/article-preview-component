9d40e55b2044e0cb2d6a8ddbc72319b9cd74fc33
Jonathan Tindungan <89713311+Nathan-Front@users.noreply.github.com>
2021-09-26 09:43:18 +0900

    Add files via upload

--- togglelinks.js
+++ togglelinks.js
@@ -1,14 +1,17 @@
 function toggleShare() {
     const links = document.getElementById('toggle-links');
+    const button = document.getElementById('clickButton');
 
     const toggleLinks = links.style.display;
 
     if (toggleLinks == 'flex') {
       // use to hide the hidden links
       links.style.display = 'none';
+      button.classList.remove('active');
     }
     else {
       // use to display the hidden links
       links.style.display = 'flex';
+      button.classList.add('active');
     }
   }
